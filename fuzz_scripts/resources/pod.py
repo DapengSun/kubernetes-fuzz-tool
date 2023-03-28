98aea701e36cf34a67abf9f7b7b6cbe9c0266228
sundapeng.sdp <sundapeng@hashdata.cn>
2023-03-28 10:49:26 +0800

    change pod fuzz yaml.

--- fuzz_scripts/resources/pod.py
+++ fuzz_scripts/resources/pod.py
@@ -44,13 +44,12 @@ class Pod(ResourceBase):
         print("pod instance: fuzz [name] start.")
         options = ResourceBase.generate_fuzz_options("%s" % self.wfuzz,
                                                      f"-z file,{self.attack_file_path} " \
-                                                     f'-z list,"{self.namespace}" ' \
                                                      f'-H Content-Type:application/json ' \
                                                      f"-d '{json.dumps(self.body, ensure_ascii=False).replace(' ', '')}' " \
                                                      f"--sc {self.fuzz_code_range} " \
                                                      f"{self.connect_delay} " \
                                                      f"{self.response_delay}",
-                                                     f"{self.kubernetes_base}{self.kubernetes_api_base}/v1/namespaces/FUZ2Z/pods?dryRun=True")
+                                                     f"{self.kubernetes_base}{self.kubernetes_api_base}/v1/namespaces/{self.namespace}/pods?dryRun=True")
 
         ResourceBase.api_caller(options)
         print("pod instance: fuzz [name] finish.")
