96a7cedb093f9a4ea815243175e7cc90b32769e9
sundapeng.sdp <sundapeng@hashdata.cn>
2023-04-03 17:42:00 +0800

    add fuzz invisable char module.

--- test_scripts/invisible_char_fuzz.py
+++ test_scripts/invisible_char_fuzz.py
@@ -65,31 +65,12 @@ def generate_fuzz_headers():
             yield new_headers
 
 
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
-
-    However pay attention at the formatting used in
-    this function because it is programmed to be pretty
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
-
-
 if __name__ == '__main__':
     for fuzz_header in generate_fuzz_headers():
         pod_metadata_path = base_dir / "fuzz_scripts/resource_metadata/actual_metadata/pod.json"
         with open(pod_metadata_path, 'r') as load_f:
             body = json.load(load_f)
         response = requests.post("http://192.168.75.100:8080", headers=fuzz_header, json=body, timeout=2)
-        prepared = response.prepare()
-        pretty_print_POST(prepared)
         # 查看响应状态码
         print(response.status_code)
         # 查看响应头部字符编码
